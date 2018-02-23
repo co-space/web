b83da95a1b9676464a6b96292e399b3cc18ee1c1
haidardzaky <haidardz@gmail.com>
2018-02-23 19:07:57 +0700

    edit little modal

--- src/small/BookModal.js
+++ src/small/BookModal.js
@@ -25,7 +25,7 @@ export default class BookModal extends React.Component {
       <div>
         <Button outline color="success" size="sm" onClick={this.toggle}>Book now !</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Room reservation</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </ModalBody>
