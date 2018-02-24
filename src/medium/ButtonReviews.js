b0c9fccc031b1ef91e94dd4c1606348569758364
ferryal <Ferryal21@gmail.com>
2018-02-24 10:34:06 +0700

    change caro on detail space using react slick

--- src/medium/ButtonReviews.js
+++ src/medium/ButtonReviews.js
@@ -5,7 +5,7 @@ import { Button } from 'reactstrap'
 const ButtonReviews = (props) => (
   <div className="btn-review">
     <Link to="/login">
-      <Button outline  color="primary" className="cta">Top Rated</Button>
+      <Button outline  color="info">Top Rated</Button>
     </Link>
   </div>
 )
