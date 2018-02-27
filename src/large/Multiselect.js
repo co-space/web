e7b1dc6491279d83741bf1186b3ccdc920e61857
ferryal <Ferryal21@gmail.com>
2018-02-27 14:11:47 +0700

    add fix data amenities & styling multiselect

--- src/large/Multiselect.js
+++ src/large/Multiselect.js
@@ -4,54 +4,56 @@ import 'react-select/dist/react-select.css';
 
 
 const FLAVOURS = [
-	{ label: 'High Speed Wifi', value: 'chocolate' },
-	{ label: 'Lounge', value: 'vanilla' },
-	{ label: 'Free Coffee/Tea', value: 'strawberry' },
-	{ label: 'Kitchen', value: 'caramel' },
-	{ label: 'Standing Desk', value: 'cookiescream' },
-	{ label: 'Pool Table', value: 'peppermint' },
-  { label: 'Skype Room', value: 'chocolate' },
-  { label: 'Projector', value: 'vanilla' },
-  { label: 'Screen', value: 'strawberry' },
-  { label: 'Large Monitor', value: 'caramel' },
-  { label: 'Speakers', value: 'cookiescream' },
-  { label: 'Personal Table', value: 'peppermint' },
-  { label: 'Computers', value: 'chocolate' },
-	{ label: 'Lounge', value: 'vanilla' },
-	{ label: 'Free Coffee/Tea', value: 'strawberry' },
-	{ label: 'Kitchen', value: 'caramel' },
-	{ label: 'Standing Desk', value: 'cookiescream' },
-	{ label: 'Pool Table', value: 'peppermint' },
-  { label: 'Skype Room', value: 'chocolate' },
-  { label: 'Projector', value: 'vanilla' },
-  { label: 'Screen', value: 'strawberry' },
-  { label: 'Large Monitor', value: 'caramel' },
-  { label: 'Speakers', value: 'cookiescream' },
-  { label: 'Personal Table', value: 'peppermint' },
+	{ label: 'High Speed Wifi 🤘🏻', value: 'highspeedwifi' },
+	{ label: 'Lounge 🤘🏻', value: 'Lounge' },
+	{ label: 'Free Coffee/Tea 🤘🏻', value: 'freecoffetea' },
+	{ label: 'Kitchen', value: 'kitchen' },
+	{ label: 'Standing Desk', value: 'standingdesk' },
+	{ label: 'Pool Table', value: 'pooltable' },
+  { label: 'Skype Room', value: 'skyperoom' },
+  { label: 'Projector', value: 'projector' },
+  { label: 'Screen', value: 'screen' },
+  { label: 'Large Monitor', value: 'largemonitor' },
+  { label: 'Speakers', value: 'speakers' },
+  { label: 'Personal Table', value: 'personaltable' },
+  { label: 'Computers (PCs)', value: 'pcs' },
+	{ label: 'Computers (Macs)', value: 'macs' },
+	{ label: 'On-site Cafe/Restaurant', value: 'restaurant' },
+	{ label: 'Comfortable Chair', value: 'chair' },
+	{ label: 'Childcare 👶🏻', value: 'childcare' },
+	{ label: 'Beanbags', value: 'beanbags' },
+  { label: 'Outdoor Teracce', value: 'outdoorteracce' },
+  { label: 'Ping Pong Table 🏓', value: 'pingpong' },
+  { label: 'Printer', value: 'printer' },
+  { label: 'Scanner', value: 'scanner' },
+  { label: '24hr member access', value: '24hr' },
+  { label: 'Dual/Single Monitor', value: 'dualsinglemonitor' },
+  { label: 'Free parking', value: 'freeparking' },
+  { label: 'Events and / or workshops', value: 'eventsandworkshops' },
+  { label: 'Whiteboard and Fresh Markers', value: 'whiteboardmarkers' },
 ];
-
-const WHY_WOULD_YOU = [
-	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
-].concat(FLAVOURS.slice(1));
+//
+// const WHY_WOULD_YOU = [
+// 	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
+// ].concat(FLAVOURS.slice(1));
+// console.log(`WHY_WOULD_YOU `, WHY_WOULD_YOU)
 
 export default class Multiselect extends React.Component {
 
   state = {
     value: [],
-    crazy: false,
+    // crazy: false,
     disabled: false,
-    removeSelected: true,
+    // removeSelected: true,
   }
   handleChange = (value) => {
     this.setState({ value });
     console.log(` You Selected: `, value);
-
-
   }
   render() {
     const { crazy, disabled, stayOpen, value } = this.state;
-		const options = crazy ? WHY_WOULD_YOU : FLAVOURS;
-
+		// const options = crazy ? WHY_WOULD_YOU : FLAVOURS;
+    const options = FLAVOURS
     return (
       <Select
         multi
@@ -59,10 +61,10 @@ export default class Multiselect extends React.Component {
         simpleValue
         rtl={this.state.rtl}
         removeSelected={true}
-        // className="Dropdown_City"
+        className="multiselect"
         value={value}
         onChange={this.handleChange}
-        placeholder="Select Your Aminieties"
+        placeholder="Select Your Amenities"
         options={options}
       />
     );
