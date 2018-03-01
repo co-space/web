31342c6fc6ad7ac3578a2886f58a45b92dae8560
haidardzaky <haidardz@gmail.com>
2018-03-01 13:52:38 +0700

    edit Amenities detail

--- src/small/AmenitiesDetail.js
+++ src/small/AmenitiesDetail.js
@@ -3,7 +3,7 @@ import {Col} from "reactstrap"
 
 const AmenitiesDetail = ({amenity}) => (
 
-    <Col sm={3} className="mb-1" id="roll-in-left">
+    <Col sm={3} className="mb-1" id="fade-in-fwd">
       <img src={`${process.env.REACT_APP_API_URL}/images/icons/checks-icon.png`} alt="check-icon"/> {amenity}
     </Col>
 
